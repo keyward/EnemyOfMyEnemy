60d1a4925d474f67f7046d6d3a8a5cc5cc943ee9
Sean McDonald <s.michael.mcdonald@gmail.com>
2016-02-07 12:15:56 -0500

    Narwhal trap start

---
+++ Assets/Scripts/Traps/Narwhal_Trap.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Narwhal_Trap : MonoBehaviour {
+
+	void OnCollisionEnter(Collision col)
+	{
+		if (col.gameObject.CompareTag ("Player")) {
+			col.gameObject.GetComponent<PlayerController> ().MoveSpeedModifier *= 0.75f;
+		}
+	}
+
+	void OnCollisionExit(Collision col)
+	{
+		if (col.gameObject.CompareTag ("Player")) {
+			col.gameObject.GetComponent<PlayerController> ().MoveSpeedModifier = 1f;
+		}
+	}
+}
