282985c1c6d53637044de5b7f3c69ec37f0bdc3b
joelthomas362 <joelthomas.362@gmail.com>
2016-02-07 14:50:50 -0500

    Updated white box level
    Added arch position triggers, updated the Ranged enemies

---
+++ Assets/Scripts/AI/RangedEnemyShot.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RangedEnemyShot : MonoBehaviour {
+
+    private RangedEnemy _rangedEnemy;
+
+
+	void Awake ()
+    {
+        _rangedEnemy = transform.parent.GetComponent<RangedEnemy>();
+	}
+	
+	
+	void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            _rangedEnemy.ShootAtPlayer();
+    }
+}
