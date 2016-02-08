f164786d15598b869c0c0333e1d5a71b602fa478
Sean McDonald <s.michael.mcdonald@gmail.com>
2016-02-08 10:00:57 -0500

    Spawn
    More dynamic spawn and animation for horn

--- Assets/Scripts/Traps/Narwhal_Trap.cs
+++ Assets/Scripts/Traps/Narwhal_Trap.cs
@@ -1,19 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Narwhal_Trap : MonoBehaviour {
+public class Narwhal_Trap : MonoBehaviour
+{
 
-	void OnCollisionEnter(Collision col)
+	public GameObject narwhal;
+	private Vector3 spawnPoint = Vector3.zero;
+
+	void OnCollisionEnter (Collision col)
 	{
 		if (col.gameObject.CompareTag ("Player")) {
-			col.gameObject.GetComponent<PlayerController> ().MoveSpeedModifier *= 0.75f;
+			col.gameObject.GetComponent<PlayerController> ().MoveSpeedModifier = 0.75f;
+			InvokeRepeating ("SpawnHorn", 2, 5);
 		}
 	}
 
-	void OnCollisionExit(Collision col)
+	void OnCollisionExit (Collision col)
 	{
 		if (col.gameObject.CompareTag ("Player")) {
 			col.gameObject.GetComponent<PlayerController> ().MoveSpeedModifier = 1f;
+			CancelInvoke();
 		}
+	}
+
+	void OnCollisionStay (Collision col)
+	{
+		spawnPoint = col.transform.position;
+	}
+
+	private void SpawnHorn ()
+	{
+		if (this.spawnPoint != Vector3.zero) {
+			spawnPoint.Set(spawnPoint.x,0,spawnPoint.z);
+			narwhal.transform.localPosition = spawnPoint;
+			Destroy (Instantiate (narwhal), 3);
+		}
+		this.spawnPoint = Vector3.zero;
 	}
 }
