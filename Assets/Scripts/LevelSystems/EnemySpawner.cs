a63e8fc9a2e3358dfcc21e635b3598ba83da1761
joelthomas362 <joelthomas.362@gmail.com>
2016-02-08 22:52:55 -0500

    Cleaned up more stuff

--- Assets/Scripts/LevelSystems/EnemySpawner.cs
+++ Assets/Scripts/LevelSystems/EnemySpawner.cs
@@ -4,24 +4,32 @@ using System.Collections;
 public class EnemySpawner : MonoBehaviour {
 
 
+
+    [Header("seconds before enemies appear")]
+    public float spawnDelay;
+
+    [Header("amount of enemies that spawn")]
     public int enemiesToSpawn;
-    public GameObject enemyPrefab;
+
+    [Header("seconds between enemy spawn")]
     public float spawnTimeFrequency = 2f;
 
-   
+    [Header("type of enemy to spawn")]
+    public GameObject enemyPrefab;
 
-	void Start ()
+
+    void Start ()
     {
         StartCoroutine(SpawnEnemy());
 	}
 	
     IEnumerator SpawnEnemy()
     {
-    
+        yield return new WaitForSeconds(spawnDelay);
+
         for(int i = 0; i < enemiesToSpawn; i++)
         {
             Instantiate(enemyPrefab, transform.position, Quaternion.identity);
-
             yield return new WaitForSeconds(spawnTimeFrequency);
         }
 
