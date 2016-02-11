c0ed5e7a28643f520973302e40b9105b5d562039
joelthomas362 <joelthomas.362@gmail.com>
2016-02-10 22:48:13 -0500

    Added Enemy Fight Scene barriers
    this adds another layer to enemy encounters, but its all encapsulated
    and cleans itself up after the fight, ill explain it in class.

---
+++ Assets/EnemyManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyManager : MonoBehaviour {
+
+
+    private LevelTrigger _levelTriggerRef;
+
+
+
+	void Awake ()
+    {
+        _levelTriggerRef = transform.parent.GetComponent<LevelTrigger>();
+	}
+	
+	public void RemoveEnemy()
+    {
+        _levelTriggerRef.MinusOneEnemy();
+    }
+}
--- Assets/Scripts/LevelSystems/LevelTrigger.cs
+++ Assets/Scripts/LevelSystems/LevelTrigger.cs
@@ -3,19 +3,35 @@ using System.Collections;
 
 public class LevelTrigger : MonoBehaviour {
 
-
+    [Header("DRAG SPAWNERS HERE")]
     public GameObject[] enemySpawners;
-    private bool triggerActivated;
 
+    [Header("DRAG BARRIER OBJECTS HERE")]
+    public Transform fightBarriers;
+
+    public float raiseSpeed;
+    public int totalEnemyCount;
+
+
+    private Transform _enemyManager;
+    private bool triggerActivated;
+    private bool _playerCrossed;
+    private bool _moeCrossed;
+
+   
     
     void Awake()
     {
         triggerActivated = false;
 
         foreach(GameObject spawner in enemySpawners)
+        {
             spawner.SetActive(false);
-    }
+            totalEnemyCount += spawner.GetComponent<EnemySpawner>().enemiesToSpawn;
+        }
 
+        _enemyManager = transform.FindChild("EnemyManager");
+    }
 
     void OnTriggerEnter(Collider other)
     {
@@ -23,11 +39,52 @@ public class LevelTrigger : MonoBehaviour {
             return;
 
         if (other.CompareTag("Player"))
+            _playerCrossed = true;
+        else if(other.CompareTag("Moe"))
+            _moeCrossed = true;
+
+
+
+        if (_playerCrossed && _moeCrossed)
         {
             triggerActivated = true;
 
             foreach (GameObject spawner in enemySpawners)
                 spawner.SetActive(true);
-        }  
+
+            StartCoroutine(RaiseBarriers());
+        }
+    }
+
+    public void MinusOneEnemy()
+    {
+        totalEnemyCount--;
+
+        if (totalEnemyCount <= 0)
+            StartCoroutine(LowerBarriers());
+
+        print(totalEnemyCount);
+    }
+
+    IEnumerator RaiseBarriers()
+    {
+        yield return new WaitForSeconds(1f);
+
+        while(fightBarriers.position.y < 2)
+        {
+            fightBarriers.position = Vector3.Lerp(fightBarriers.position, fightBarriers.position + (Vector3.up * 3), Time.deltaTime * raiseSpeed);
+            yield return null;
+        }
+    }
+
+    IEnumerator LowerBarriers()
+    {
+        while (fightBarriers.position.y > -1)
+        {
+            fightBarriers.position = Vector3.Lerp(fightBarriers.position, fightBarriers.position + (Vector3.down * 3), Time.deltaTime * raiseSpeed);
+            yield return null;
+        }
+        Destroy(fightBarriers.gameObject);
+        Destroy(gameObject);
     }
 }
