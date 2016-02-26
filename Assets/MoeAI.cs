5f96a40e85afd2c6c91b45e4cc771f8e28f0c397
joelthomas362 <joelthomas.362@gmail.com>
2016-02-26 08:35:57 -0500

    Good Moe State Machine

--- Assets/MoeAI.cs
+++ Assets/MoeAI.cs
@@ -1,29 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MoeTest : MonoBehaviour {
+public class MoeAI : MonoBehaviour {
 
 
     enum aiState { following, attacking, charging, stoned };
-
     aiState currentState;
 
+
+    // MoeAI attacks
+    private GameObject _areaDamage;
+    private GameObject _chargeDamage;
+
+    // MoeAI pathfinding
     private Transform _playerTransform;
     private Vector3 _lastPlayerLocation;
     private NavMeshAgent _navAgent;
 
     private bool _attacking;
+    private bool _frozen;
 
 	void Awake ()
     {
         currentState = aiState.following;
 
+        _areaDamage = transform.GetChild(0).gameObject;
+        _chargeDamage = transform.GetChild(1).gameObject;
+        _areaDamage.SetActive(false);
+        _chargeDamage.SetActive(false);
+
+
         _playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
         _navAgent = GetComponent<NavMeshAgent>();
 
-
         _attacking = false;
+        _frozen = false;
 
+        // -- Drives the entire State Machine -- //
         InvokeRepeating("StateLogic", 0f, .1f);
 	}
 	
@@ -35,33 +48,40 @@ public class MoeTest : MonoBehaviour {
 
         else if (Input.GetKeyDown(KeyCode.S))
             currentState = aiState.charging;
+
+        else if (Input.GetKeyDown(KeyCode.D))
+            currentState = aiState.stoned;
 	}
 
     void StateLogic()
     {
-        //print("checking states");
         switch(currentState)
         {
             case aiState.following:
                 Follow();
                 break;
+
             case aiState.attacking:
                 StartCoroutine(Attack());
                 break;
+
             case aiState.charging:
                 StartCoroutine(Charge());
                 break;
+
             case aiState.stoned:
-                TurnToStone();
+                StartCoroutine(TurnToStone());
                 break;
+
             default:
-                print("whatever");
+                Follow();
                 break;
         }
     }
 
     void Follow()
     {
+        // stops 5 meters from player //
         if (Vector3.Distance(transform.position, _playerTransform.position) > 5f)
         {
             if(_navAgent.velocity == Vector3.zero)
@@ -71,13 +91,12 @@ public class MoeTest : MonoBehaviour {
         }  
         else
             _navAgent.Stop();
-
-        //print("following");
     }
 
+    // -- Area Attack -- //
     IEnumerator Attack()
     {
-        if (_attacking)
+        if (_attacking || _frozen)
             yield break;
 
         _attacking = true;
@@ -86,6 +105,11 @@ public class MoeTest : MonoBehaviour {
             _navAgent.Stop();
 
         print("attacking");
+        _areaDamage.SetActive(true);
+        yield return new WaitForSeconds(.1f);
+        _areaDamage.SetActive(false);
+        // play animation "Attack"
+        // Instantiate particles
 
         yield return new WaitForSeconds(1f);
 
@@ -93,14 +117,13 @@ public class MoeTest : MonoBehaviour {
         _attacking = false;
     }
 
+    // -- Charge at player -- //
     IEnumerator Charge()
     {
-        if (_attacking)
+        if (_attacking || _frozen)
             yield break;
 
         _attacking = true;
-        print("starting charge");
-
 
         // get initial values
         float normalSpeed = _navAgent.speed;
@@ -115,6 +138,8 @@ public class MoeTest : MonoBehaviour {
 
         yield return new WaitForSeconds(.2f);
 
+        _chargeDamage.SetActive(true);
+
         //set charge speed
         _navAgent.speed = 50f;
         _navAgent.acceleration = 100f;
@@ -124,9 +149,17 @@ public class MoeTest : MonoBehaviour {
 
         // charge at players last position
         while(_navAgent.remainingDistance > .01f)
+        {
+            // if stoned in the middle of a charge -- cancel the charge
+            if (currentState == aiState.stoned)
+                break;
+ 
+            // perform charge
             yield return null;
-        
+        }
+
         // reset navAgent values to inital
+        _chargeDamage.SetActive(false);
         _navAgent.ResetPath();
         _navAgent.speed = normalSpeed;
         _navAgent.acceleration = normalAcceleration;
@@ -134,17 +167,30 @@ public class MoeTest : MonoBehaviour {
         _navAgent.stoppingDistance = normalStoppingDistance;
 
         yield return new WaitForSeconds(.75f);
-        print("charge over");
 
         // reset ai state
         _attacking = false;
         currentState = aiState.following;
     }
 
-    void TurnToStone()
+    // -- Halts Moe's position, and resets attack -- //
+    IEnumerator TurnToStone()
     {
-        print("stoned");
-        // stop moving for a second, if charging stopDestination
+        if (_frozen)
+            yield break;
+
+        _frozen = true;
+
+        // swap texture to stone texture
+        _navAgent.velocity = Vector3.zero;
+        _navAgent.Stop();
+
+        yield return new WaitForSeconds(1f);
+
+        // swap texture to initial texture
+        // continue following player
+        currentState = aiState.following;
+        _frozen = false;
     }
 
     void OnTriggerEnter(Collider other)
