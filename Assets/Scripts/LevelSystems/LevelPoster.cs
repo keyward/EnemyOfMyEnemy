5ad20edbaacd0b0c5cf023ff1465a720ccb441db
joelthomas362 <joelthomas.362@gmail.com>
2016-03-01 21:34:23 -0500

    Poster Functionality

--- Assets/Scripts/LevelSystems/LevelPoster.cs
+++ Assets/Scripts/LevelSystems/LevelPoster.cs
@@ -7,34 +7,56 @@ public class LevelPoster : MonoBehaviour {
 
     // get canvas by tag
     // Insert Img to display
-    public Image posterToDisplay;
+    public Sprite posterToDisplay;
 
     private GameObject _posterCanvas;
     private Image _canvasImage;
     private bool _posterTurnedOn;
+    private bool _inputEnabled;
     
 
     void Awake()
     {
         _posterCanvas = GameObject.FindGameObjectWithTag("PosterCanvas");
-        _canvasImage = _posterCanvas.transform.GetChild(2).GetComponent<Image>();
+        _canvasImage = _posterCanvas.transform.FindChild("PosterImage").GetComponent<Image>();
+        print(_posterCanvas);
 
-        _posterCanvas.SetActive(false);
         _posterTurnedOn = false;
+        _inputEnabled = false;
     }
 
-    void OnTriggerStay(Collider other)
+    void Start()
     {
-        if (Input.GetButtonDown("Fire3"))
+        if (_posterCanvas.activeInHierarchy)
+            _posterCanvas.SetActive(false);
+    }
+
+    void Update()
+    {
+        if(_inputEnabled && Input.GetButtonDown("Fire2"))
         {
             _posterTurnedOn = !_posterTurnedOn;
-            _posterCanvas.SetActive(_posterTurnedOn);
-            _canvasImage = posterToDisplay;
+            _posterCanvas.SetActive(_posterTurnedOn); 
         }
     }
+
+    // when player walks up to the poster - slot the correct image, and allow them to activate the canvas
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _canvasImage.sprite = posterToDisplay;
+            _inputEnabled = true;
+        }  
+    }
     
+    // when player leaves - disable the canvas and input
     void OnTriggerExit(Collider other)
     {
-        _posterCanvas.SetActive(false);
+        if(other.CompareTag("Player"))
+        {
+            _inputEnabled = false;
+            _posterCanvas.SetActive(false);
+        }
     }
 }
