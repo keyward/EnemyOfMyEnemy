db9e0a1ff0141b6f52544c19405801fece0b8b2d
joelthomas362 <joelthomas.362@gmail.com>
2016-04-02 17:34:50 -0400

    Fire Deals Damage Now

--- Assets/Scripts/Traps/DragonFlame.cs
+++ Assets/Scripts/Traps/DragonFlame.cs
@@ -5,20 +5,21 @@ public class DragonFlame : MonoBehaviour
 {
 
 	#region Components
-	public GameObject fireDamage;
 	public GameObject fireParticles;
-	public float _Angle;
+    [Range(0, 45)]
+    public float _Angle;
 	public float _Period;
 	private float _Time;
-	private Color initialColor;
-	private Vector3 startPos;
+
 	private GameObject instantiatedFireParticles;
 	private bool runCoroutine = true;
-	#endregion
-	// Use this for initialization
-	
-	// Update is called once per frame
-	void Update ()
+    #endregion
+
+    //public GameObject fireDamage;
+    //private Color initialColor;
+    //private Vector3 startPos;
+
+    void Update ()
 	{
 		if (this.runCoroutine) {
 			StartCoroutine (InstantiateParticle ());
@@ -36,16 +37,11 @@ public class DragonFlame : MonoBehaviour
 	{
 		this.runCoroutine = false;
 		if (this.instantiatedFireParticles != null) {
-			Destroy (this.instantiatedFireParticles);
+			//Destroy (this.instantiatedFireParticles);
 		} else {
 			instantiatedFireParticles = Instantiate (fireParticles, transform.position, Quaternion.Euler (0f, this.transform.rotation.eulerAngles.y, 0f)) as GameObject;
 		}
 		yield return new WaitForSeconds (4);
 		this.runCoroutine = true;
-	}
-
-	void OnParticleCollision (GameObject other)
-	{
-
 	}
 }
