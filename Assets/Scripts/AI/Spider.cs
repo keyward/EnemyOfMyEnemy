8d05a6460758644f87a9a48f4e70ec4a181d7f86
joelthomas362 <joelthomas.362@gmail.com>
2016-04-05 22:51:37 -0400

    Revert "Level 1 quick fix"
    This reverts commit de990ef30569d6b91c13c3077982ebd0d198ee29.

--- Assets/Scripts/AI/Spider.cs
+++ Assets/Scripts/AI/Spider.cs
@@ -44,7 +44,7 @@ public class Spider : MonoBehaviour {
         {
             // get a random position while staying within bounds //
             Vector3 nextPosition = new Vector3(Random.Range(_spawnLocation.x - 10f, _spawnLocation.x + 10f),   //x
-                                               _spawnLocation.y,                                               //y
+                                               2f,                                                             //y
                                                Random.Range(_spawnLocation.z - 10f, _spawnLocation.z + 10f));  //z
 
             // lerp to position //
