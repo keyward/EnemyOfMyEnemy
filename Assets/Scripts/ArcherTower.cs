5bd18b79ff8af26465e5689e935bb5d2142c685d
joelthomas362 <joelthomas.362@gmail.com>
2016-04-15 21:45:28 -0400

    Player Can destroy archer tower now

--- Assets/Scripts/ArcherTower.cs
+++ Assets/Scripts/ArcherTower.cs
@@ -3,10 +3,33 @@ using System.Collections;
 
 public class ArcherTower : MonoBehaviour {
 
+    [Header("Put Archer Here")]
     public Rigidbody archerOnTower;
+
+    [Header("Death Properties")]
+    public GameObject deathParticles;
+    private AudioSource _deathSound;
+
+    private Health _towerHealth;
+
+    void Start()
+    {
+        _deathSound = GetComponent<AudioSource>();
+        _towerHealth = GetComponent<Health>();
+    }
 
     void OnDestroy()
     {
         archerOnTower.isKinematic = false;
+        _deathSound.Play();
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.CompareTag("Bullet"))
+        {
+            Instantiate(deathParticles, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
     }
 }
