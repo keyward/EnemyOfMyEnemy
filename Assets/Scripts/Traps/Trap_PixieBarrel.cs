52e375115782a94dc2d1fc599956b57439fec343
joelthomas362 <joelthomas.362@gmail.com>
2016-04-15 22:14:44 -0400

    Pixie Barrel

---
+++ Assets/Scripts/Traps/Trap_PixieBarrel.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trap_PixieBarrel : MonoBehaviour {
+
+    public GameObject pixieSpawner;
+
+    void Awake()
+    {
+        pixieSpawner.SetActive(false);
+    }
+
+	void OnDestroy()
+    {
+        pixieSpawner.SetActive(true);
+    }
+}
